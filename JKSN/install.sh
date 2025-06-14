a83c5966d46a486f9602fa39217f3074617f5bb3
andy <andy@thejacksons.us>
2025-06-14 14:29:57 -0400

    fixed install script and service file

--- JKSN/install.sh
+++ JKSN/install.sh
@@ -5,8 +5,30 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 version_check () {
+    if [[ ! $(command -v dotnet >/dev/null 2>&1) ]]; then
+        return 0
+    fi
     version=$(dotnet --version)
     printf '%s\n%s\n' "9.0.0" "$version" | sort --check=quiet --version-sort
+}
+
+make_dir () {
+    echo "Creating directory $1"
+    mkdir "$1"
+}
+
+update_permissions () {
+    echo "Settings permissions for $1"
+    echo "Setting owner to jksn."
+    chown jksn: -R "$1"
+    echo "Adding read permissions for all."
+    chmod a+r -R "$1"
+    echo "Removing write permissions for other."
+    chmod o-w -R "$1"
+    echo "Adding write permissions to jksn:jksn."
+    chmod ug+w -R "$1"
+    echo "Adding executable to folders for all."
+    chmod a+X -R "$1"
 }
 
 echo "Looking for dotnet."
@@ -16,16 +38,16 @@ then
 else
     echo "SDK dotnet not found or version is not 9.0.0 or higher."
     source /etc/lsb-release
-    if [[ "$DISTRIB_RELEASE" == "24.04" || "$DISTRIB_RELEASE" == "22.04"]]; then
+    if [[ "$DISTRIB_RELEASE" == "24.04" || "$DISTRIB_RELEASE" == "22.04" ]]; then
         echo "Ubuntu $DISTRIB_RELEASE $DISTRIB_CODENAME detected, installing dotnet 9.0 SDK."
         echo "Adding repository."
         add-apt-repository ppa:dotnet/backports
         echo "repository added."
-    elif [[ "$DISTRIB_RELEASE" -eq "24.10" ]]; then
+    elif [[ "$DISTRIB_RELEASE" == "24.10" ]]; then
         echo "Adding repository."
         echo "Ubuntu $DISTRIB_RELEASE $DISTRIB_CODENAME detected, installing dotnet 9.0 SDK."
         echo "Downloading the repository package."
-        curl -sSL -O https://packages.microsoft.com/config/ubuntu/$DISTRIB_RELEASE/packages-microsoft-prod.deb
+        curl -sSL -O https://packages.microsoft.com/config/ubuntu/"$DISTRIB_RELEASE"/packages-microsoft-prod.deb
         echo "Download complete."
         echo "Deploying the repository package."
         dpkg -i packages-microsoft-prod.deb
@@ -65,37 +87,28 @@ rm jksn-1.0.0.tar.gz
 
 echo "Installing jksn."
 
-echo "Creating '/var/jksn' directory."
-mkdir /var/jksn
-echo "Creating '/etc/jksn' directory."
-mkdir /etc/jksn
-echo "Creating '/opt/jksn' directory.";
-mkdir /opt/jksn
+make_dir "/var/jksn"
+make_dir "/etc/jksn"
+make_dir "/opt/jksn"
 
 echo "Copying binarries into '/opt/jksn'."
 cp jksn-1.0.0/binaries/* /opt/jksn/
 echo "Creating symlink into '/usr/bin'."
 ln -s /opt/jksn/JKSN /usr/bin/JKSN
 
-echo "Adding executable permissions for jksn."
-chmod o+x /opt/jksn/JKSN
-chmod g+x /opt/jksn/JKSN
+echo "Adding executable permissions for jksn:jksn and removing for other."
+chmod ug+x /opt/jksn/JKSN
+chmod o-x /opt/jksn/JKSN
 
 echo "Creating initial configuration file".
 cp jksn-1.0.0/config/config.toml /etc/jksn/config.toml
 
-echo "Changing ownership of files to 'jksn:jksn'."
-chown -R jksn:jksn /opt/jksn
-chown -R jksn:jksn /etc/jksn
-chown -R jksn:jksn /var/jksn
-
-echo "Setting permissions for '/var/jksn' to 770:660."
-chmod -R u-rwx /opt/jksn
-chmod -R u-rwx /etc/jksn
-chmod -R u-rwx /var/jksn
+update_permissions "/opt/jksn"
+update_permissions "/etc/jksn"
+update_permissions "/var/jksn"
 
 echo "Creating systemd file."
-cp systemd/jksn.service /lib/systemd/system/jksn.service
+cp jksn-1.0.0/systemd/jksn.service /lib/systemd/system/jksn.service
 
 echo "Updating daemon."
 systemctl daemon-reload
@@ -106,4 +119,6 @@ systemctl enable jksn
 echo "Starting service."
 service jksn start
 
+echo "Removing intall files."
+rm -r jksn-1.0.0
 echo "Installation finished."
